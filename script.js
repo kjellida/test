d534818404c61a04f0dc17f9376c35c29977395c
kjellida <ida.kjellerstedt@hotmail.com>
2025-09-14 20:34:29 +0200

    changed colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
     const p = document.createElement("p");
-    p.innerText = "and it's dynamic, too!";
+    p.innerText = "Trying javascript as well!";
     document.body.appendChild(p);
   }, 2000);
